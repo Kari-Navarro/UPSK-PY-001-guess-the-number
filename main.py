e8c27952f1df5be926761054c52e6bb486cbb377
Karina Navarro <karinavarro.b@gmail.com>
2024-02-20 12:50:15 -0600

    changelog added and new file for test

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ import random
 #Sugerencias de escritura crtl + barra espaciadora
 
 user_name = input("Write your name: ")
-print("*"*30,"Welcome to the Guess the Number game", user_name,"!","*"*30)
+print("*"*10,"Welcome to the Guess the Number game", user_name,"!","*"*10)
 random_number = random.randint(1,100)
 lower_limit = 1
 higher_limit = 100
 player_number = -1
 pc = -1
-
+pc_allnumbers = []
+player_allnumbers = []
+rounds = 0
 
 
 while player_number != random_number and pc != random_number:
     try:
-        print("     PC, guess the number between 1 and 100")
+        rounds = rounds + 1
+        print("PC, guess the number between 1 and 100")
         pc = random.randint(lower_limit,higher_limit)
+        pc_allnumbers.append(pc)
         print(f"PC: {pc}")
         if pc < random_number:
             print("Too low! Try again")
@@ -28,9 +32,12 @@ while player_number != random_number and pc != random_number:
             higher_limit = pc - 1
         else:
             print("-----The winner is PC-----")
+            print("All numbers from PC: ", pc_allnumbers)
+            print("All numbers from ", user_name,": ", player_allnumbers)
+            
         if pc != random_number:
             player_number = int(input(user_name + ", guess the number between 1 and 100: "))
-        
+            player_allnumbers.append(player_number)
             if player_number < random_number:
                 print("Too low! Try again")
                 if player_number > lower_limit:
@@ -41,6 +48,10 @@ while player_number != random_number and pc != random_number:
                     higher_limit = player_number - 1
             else:
                 print("Congratulations," + user_name + " won!")
+                print("All numbers from PC: ", pc_allnumbers)
+                print("All numbers from ", user_name,": ", player_allnumbers)
+
                 break
     except ValueError:
         print("Enter only integer numbers")
+print("Attempts:",rounds)
